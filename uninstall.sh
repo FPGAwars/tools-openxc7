1660afac8e7128180d81b3f1e5e5a4e06fa52fe2
obijuan <obijuan.cube@gmail.com>
2026-05-28 11:47:13 +0200

    Add install and uninstall scripts

---
+++ uninstall.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# ----------------------------------------------
+# -- USER CONFIGURATION
+# ----------------------------------------------
+# -- Local path where to store the tools-openxc7 package
+OPENXC7_INSTALL_PATH=$HOME/.local
+
+# -----------------------------------------------
+# -- END OF USER CONFIGURATION
+# -----------------------------------------------
+
+OPENXC7=$OPENXC7_INSTALL_PATH/openxc7
+
+printf "🔵 Borrando carpeta: $OPENXC7\n"
+chmod -Rf +w $OPENXC7
+rm -rf $OPENXC7
+printf "\n"
+
+
